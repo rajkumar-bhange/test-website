4eb09474c5567f3475fe2d0adbaebc3a370980d8
rajkumar-bhange <rajkumarbh45@gmail.com>
2025-10-21 12:17:32 +0530

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+});
