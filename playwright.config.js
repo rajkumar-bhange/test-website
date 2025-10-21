ed05794b40f58173b6cebe87231a47fc5a3a5861
rajkumar-bhange <rajkumarbh45@gmail.com>
2025-10-21 12:21:49 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  // Use the root directory for tests
+  testDir: '.',  
+
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  
   use: {
     headless: true,
-    viewport: { width: 1280, height: 720 },
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
